59e8a663790daff645c97fb0088b853b18145ade
Nguyễn Ngọc Chiến <56423606+Cotchi666@users.noreply.github.com>
2022-08-11 10:47:30 +0700

    update

--- src/components/home/recent/RecentCard.jsx
+++ src/components/home/recent/RecentCard.jsx
@@ -1,70 +1,98 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import houseApi from "../../../api/houseApi";
+import logger from "use-reducer-logger";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, rooms: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 const RecentCard = () => {
-  const [data, setHouseList] = useState([]);
-  //form raw 3 properties
+  const [{ rooms, loading, error }, dispatch] = useReducer(logger(reducer), {
+    rooms: [],
+    loading: true,
+    error: "",
+  });
+  // const [rooms, setRooms] = useState([]);
+
   useEffect(() => {
-    const fetchHouseList = async () => {
+    const fetchRooms = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
       try {
         const response = await houseApi.getAllRoom();
         console.log("Fetch products successfully: ", response.results);
-        // 3 out of 8 obj in an array
-        const apidata = response.results.slice(0, 9);
-        console.log("check from list property ", apidata);
-        setHouseList(apidata);
+        // setRooms(response.results.slice(0, 9));
+        dispatch({
+          type: "FETCH_SUCCESS",
+          payload: response.results.slice(0, 9),
+        });
       } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: error.message });
         console.log("Failed to fetch product list: ", error);
       }
     };
-    fetchHouseList();
+    fetchRooms();
   }, []);
   return (
     <>
       <div className="content grid3 mtop">
-        {data.map((item) => {
-          const category = item.parent.CategoryId.categories;
-          return (
-            <div className="box shadow" key={item.objectId}>
-              <Link to href={`/classes/Room/${item.objectId}`}>
-                <div className="img">
-                  <img src={item.parent.image} alt="" />
-                </div>
-              </Link>
-              <div className="text">
-                <div className="category flex">
-                  <span
-                    style={{
-                      background:
-                        category === "Popular" ? "#25b5791a" : "#ff98001a",
-                      color: category === "Popular" ? "#25b579" : "#ff9800",
-                    }}
-                  >
-                    {item.parent.CategoryId.categories}
-                  </span>
-                  <i className="fa fa-heart"></i>
-                </div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          rooms.map((item) => {
+            const category = item.parent.CategoryId.categories;
+            return (
+              <div className="box shadow" key={item.objectId}>
                 <Link to href={`/classes/Room/${item.objectId}`}>
-                  <h4>{item.name}</h4>
+                  <div className="img">
+                    <img src={item.parent.image} alt="" />
+                  </div>
                 </Link>
-                <p>
-                  <i className="fa fa-location-dot"></i> {item.parent.location}
-                </p>
-              </div>
-              <div className="button flex">
-                <div>
+                <div className="text">
+                  <div className="category flex">
+                    <span
+                      style={{
+                        background:
+                          category === "Popular" ? "#25b5791a" : "#ff98001a",
+                        color: category === "Popular" ? "#25b579" : "#ff9800",
+                      }}
+                    >
+                      {item.parent.CategoryId.categories}
+                    </span>
+                    <i className="fa fa-heart"></i>
+                  </div>
                   <Link to href={`/classes/Room/${item.objectId}`}>
-                    <button className="btn2">{item.parent.price}</button>
+                    <h4>{item.name}</h4>
                   </Link>
-
-                  <label htmlFor="">/sqft</label>
+                  <p>
+                    <i className="fa fa-location-dot"></i>{" "}
+                    {item.parent.location}
+                  </p>
                 </div>
-                <span>{item.parent.CategoryId.categories}</span>
+                <div className="button flex">
+                  <div>
+                    <Link to href={`/classes/Room/${item.objectId}`}>
+                      <button className="btn2">{item.parent.price}</button>
+                    </Link>
+
+                    <label htmlFor="">/sqft</label>
+                  </div>
+                  <span>{item.parent.CategoryId.categories}</span>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
     </>
   );
